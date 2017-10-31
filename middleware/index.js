a9a217adbfb274b706df0131293ad1575ce01f92
sbrocks <shaury29.baranwal@gmail.com>
2017-10-31 19:52:50 +0530

    admin

--- middleware/index.js
+++ middleware/index.js
@@ -13,7 +13,7 @@ middlewareObj.checkCampgroundOwnership = function(req,res,next){
 				res.redirect('back');
 			}else{
 				//does user own the campground?
-				if(foundCampground.author.id.equals(req.user._id)){
+				if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin){
 					next();
 				}else{
 					req.flash("error","You don't have permission to do that!");
--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,9 @@ router.get('/register',function(req,res){
 // handle sign up logic
 router.post('/register',function(req,res){
 	var newUser= new User({username:req.body.username});
+	if(req.body.adminCode === 'secretcode123'){
+		newUser.isAdmin=true;
+	}
 	User.register(newUser,req.body.password,function(err,user){
 		if(err){
 			req.flash("error", err.message);
